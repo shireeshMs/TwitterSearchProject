4c13f25cf0aaab998950885fb1281e3c519fa9b6
Shireesh <PiancaSiri@shireeshs-macbook-air.local>
2018-03-10 15:36:19 +0530

    Initial Commit

---
+++ TwitterSearchProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterSearchProject
+//
+//  Created by PiancaSiri on 10/03/18.
+//  Copyright © 2018 PiancaSiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
